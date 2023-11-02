333a4843a23b42d203b81831c38fc6b5c26fa6cb
Jose Vicente Nunez <josevnz@kodegeek.com>
2023-11-01 20:44:45 -0400

    New code that shows how to perform certain tasks with Textualize

---
+++ docs/Textualize/README.md
@@ -0,0 +1,31 @@
+# Textualize
+
+This is my collection of small, self-contained applications that use Textualize to demonstrate features.
+
+You must create a virtual environment in order to run the examples:
+
+```shell
+python3 -m venv ~/virtualenv/Textualize && . ~/virtualenv/Textualize/bin/activate
+pip install --upgrade pip
+pip install --upgrade wheel
+pip install -r requirements.txt
+```
+
+Code may work with Python 3.7, but I used Python 3.10 while writing the code.
+
+## [Table with detail screen](table_with_detail_screen.py)
+
+Shows a table, with sortable columns. When you click in a row, you get more details.
+
+To run:
+```shell
+# Terminal 1
+. ~/virtualenv/Textualize/bin/activate
+textual console
+```
+
+```shell
+# Terminal 2
+. ~/virtualenv/Textualize/bin/activate
+textual run --dev table_with_detail_screen.py
+```
---
+++ docs/Textualize/table_with_detail_screen.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+"""
+Author: Jose Vicente Nunez
+"""
+from textual import on
+from textual.app import ComposeResult, App
+from textual.screen import ModalScreen
+from textual.widgets import DataTable, Footer, Header, Button, MarkdownViewer
+
+MY_DATA = [
+    ("level", "name", "gender", "country", "age"),
+    ("Green", "Wai", "M", "MYS", 22),
+    ("Red", "Ryoji", "M", "JPN", 30),
+    ("Purple", "Fabio", "M", "ITA", 99),
+    ("Blue", "Manuela", "F", "VEN", 25)
+]
+
+
+class DetailScreen(ModalScreen):
+    ENABLE_COMMAND_PALETTE = False
+    CSS = """
+    MarkdownViewer {
+        dock: top;
+        width: 100%;
+        height: auto;
+    }
+
+    Button {
+        dock: bottom;
+        width: 100%;
+        height: auto;
+    }
+    """
+
+    def __init__(
+            self,
+            name: str | None = None,
+            ident: str | None = None,
+            classes: str | None = None,
+            row_selected: DataTable.RowSelected = None
+    ):
+        super().__init__(name, ident, classes)
+        self.row_selected = row_selected
+
+    def compose(self) -> ComposeResult:
+        self.log.info(f"Details: {self.row_selected}")
+        table = self.row_selected.data_table
+        row_key = self.row_selected.row_key
+        row = table.get_row(row_key)
+        columns = MY_DATA[0]
+        row_markdown = "\n"
+        for i in range(0, len(columns)):
+            row_markdown += f"* **{columns[i].title()}:** {row[i]}\n"
+        yield MarkdownViewer(f"""## User details:
+        {row_markdown}
+        """)
+        yield Button("Close", variant="primary", id="close")
+
+    @on(Button.Pressed, "#close")
+    def on_button_pressed(self, _) -> None:
+        self.app.pop_screen()
+
+
+class MyApp(App):
+    BINDINGS = [
+        ("q", "quit_app", "Quit"),
+    ]
+    CSS = """
+    Screen {
+        layout: vertical;
+    }
+    
+    Header {
+        height: 3;
+        dock: top;
+    }
+
+    Footer {
+        height: 1;
+        dock: bottom;
+    }
+
+    DataTable {
+        width: 100;
+        height: 100%;
+        border: solid green;
+    }
+    """
+    ENABLE_COMMAND_PALETTE = False
+
+    def action_quit_app(self):
+        self.exit(0)
+
+    def compose(self) -> ComposeResult:
+        yield Header(show_clock=True)
+        table = DataTable(id=f'table_myapp')
+        table.cursor_type = 'row'
+        table.zebra_stripes = True
+        yield table
+        yield Footer()
+
+    def on_mount(self) -> None:
+        table = self.get_widget_by_id(f'table_myapp', expect_type=DataTable)
+        columns = [x.title() for x in MY_DATA[0]]
+        table.add_columns(*columns)
+        table.add_rows(MY_DATA[1:])
+
+    @on(DataTable.HeaderSelected)
+    def on_header_clicked(self, event: DataTable.HeaderSelected):
+        table = event.data_table
+        table.sort(event.column_key)
+
+    @on(DataTable.RowSelected)
+    def on_row_clicked(self, event: DataTable.RowSelected) -> None:
+        runner_detail = DetailScreen(row_selected=event)
+        self.push_screen(runner_detail)
+
+
+def run_myapp():
+    app = MyApp()
+    app.title = f"Summary".title()
+    app.sub_title = f"{len(MY_DATA)} users"
+    app.run()
+
+
+if __name__ == "__main__":
+    run_myapp()
