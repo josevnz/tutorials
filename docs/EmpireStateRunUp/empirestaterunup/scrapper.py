e6527b06ed3cec8335b6a4369d08f4ae0fd52e76
Jose Vicente Nunez <josevnz@kodegeek.com>
2023-11-14 21:10:08 -0500

    Finished racer link scraping, skeleton for racer details

--- docs/EmpireStateRunUp/empirestaterunup/scrapper.py
+++ docs/EmpireStateRunUp/empirestaterunup/scrapper.py
@@ -1,6 +1,6 @@
 import re
 from time import sleep
-from typing import Dict, Any
+from typing import Any
 
 from selenium import webdriver
 from selenium.common import NoSuchElementException
@@ -22,9 +22,27 @@ class RacerLinksScrapper:
         self.load_wait = load_wait
         self.driver.get(EMPIRE_STATE_2013_RACE_RESULTS)
         sleep(self.load_wait)
-        self.__links = {}
+        self.links = {}
 
     def __enter__(self):
+        try:
+            self.__get_racer_links__()
+            self.__click__(6)
+            self.__get_racer_links__()
+            self.__click__(7)
+            self.__get_racer_links__()
+            self.__click__(7)
+            self.__get_racer_links__()
+            self.__click__(9)
+            self.__get_racer_links__()
+            self.__click__(9)
+            self.__get_racer_links__()
+            self.__click__(7)
+            self.__get_racer_links__()
+            self.__click__(7)
+            self.__get_racer_links__()
+        except NoSuchElementException as nse:
+            raise ValueError("Error finding race details", nse)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -35,7 +53,7 @@ class RacerLinksScrapper:
             href = a.get_attribute('href')
             if re.search('Bib', href):
                 name = a.text.strip().title()
-                self.__links[name] = href.strip()
+                self.links[name] = href.strip()
 
     def __click__(self, level: int) -> Any:
         button = WebDriverWait(self.driver, 20).until(
@@ -44,20 +62,21 @@ class RacerLinksScrapper:
         sleep(2.5)
         return button
 
-    def get_all_links(self) -> Dict[str, str]:
-        self.__get_racer_links__()
-        self.__click__(6)
-        self.__get_racer_links__()
-        self.__click__(7)
-        self.__get_racer_links__()
-        self.__click__(7)
-        self.__get_racer_links__()
-        self.__click__(9)
-        self.__get_racer_links__()
-        self.__click__(9)
-        self.__get_racer_links__()
-        self.__click__(7)
-        self.__get_racer_links__()
-        self.__click__(7)
-        self.__get_racer_links__()
-        return self.__links
+
+class RacerDetailsScrapper:
+
+    def __init__(self, url: str, name: str, headless: bool = True, load_wait: int = 5):
+        options = Options()
+        if headless:
+            options.add_argument("--headless")
+        self.driver = webdriver.Firefox(options=options)
+        self.load_wait = load_wait
+        self.driver.get(url)
+        sleep(self.load_wait)
+        self.name = name
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.driver.close()
--- docs/EmpireStateRunUp/test/test_scrapper.py
+++ docs/EmpireStateRunUp/test/test_scrapper.py
@@ -2,7 +2,7 @@ import logging
 import pprint
 import unittest
 
-from empirestaterunup.scrapper import RacerLinksScrapper
+from empirestaterunup.scrapper import RacerLinksScrapper, RacerDetailsScrapper
 
 logger = logging.getLogger('selenium')
 logger.setLevel(logging.DEBUG)
@@ -12,9 +12,20 @@ class RacerLinksScrapperTestCase(unittest.TestCase):
     def test_navigate(self):
         with RacerLinksScrapper(headless=True) as esc:
             self.assertIsNotNone(esc)
-            links = esc.get_all_links()
-            pprint.pprint(links)
-            self.assertEqual(377, len(links))
+            pprint.pprint(esc.links)
+            self.assertEqual(377, len(esc.links))
+
+    def test_runner_detail(self):
+        links = {
+            'Aaron Field': 'https://www.athlinks.com/event/382111/results/Event/1062909/Course/2407855/Bib/368',
+            'Al Bueno': 'https://www.athlinks.com/event/382111/results/Event/1062909/Course/2407855/Bib/609',
+            'Alan Lynch': 'https://www.athlinks.com/event/382111/results/Event/1062909/Course/2407855/Bib/57',
+            'Alejandra Sanchez': 'https://www.athlinks.com/event/382111/results/Event/1062909/Course/2407855/Bib/40',
+            'Alessandro Manrique': 'https://www.athlinks.com/event/382111/results/Event/1062909/Course/2407855/Bib/562'
+        }
+        for name in links:
+            with RacerDetailsScrapper(links[name], name) as rds:
+                self.assertIsNotNone(rds)
 
 
 if __name__ == '__main__':
