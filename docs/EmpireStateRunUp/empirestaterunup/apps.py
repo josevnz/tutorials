bf8ab8c3e67fa06fabef3b28b8e44870c227bd9d
Jose Vicente Nunez <josevnz@kodegeek.com>
2023-12-22 16:41:31 -0500

    Enrich and beautify plots

--- docs/EmpireStateRunUp/empirestaterunup/apps.py
+++ docs/EmpireStateRunUp/empirestaterunup/apps.py
@@ -265,6 +265,7 @@ class Plotter:
             series = self.df[RaceFields.age.value]
             fig, ax = plt.subplots(layout='constrained')
             n, bins, patches = ax.hist(series, density=False, alpha=0.75)
+            # Borrowed coloring recipe for histogram from their documentation
             fracs = n / n.max()
             norm = colors.Normalize(fracs.min(), fracs.max())
             for frac, patch in zip(fracs, patches):
